8161afabb863f4c49a8abb439eb7c53dabbb9b4f
luty <luty@luty.info>
2023-09-07 21:11:40 +0900

    first commmit

---
+++ CSharpSummaryShortener/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace CSharpSummaryShortener
+{
+    internal class Program
+    {
+        public class Block
+        {
+            public bool IsSummary { get; set; } = false;
+            public string? Indent { get; set; }
+            public List<string> SummaryBodyLines { get; set; } = new();
+            public List<string> RawLines { get; set; } = new();
+            public string Render()
+            {
+                if (IsSummary && SummaryBodyLines.Count == 1)
+                {
+                    return $"{Indent}/// <summary> {SummaryBodyLines.Single()} </summary>";
+                }
+                else
+                {
+                    return string.Join("\n", RawLines);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var inputFilePath = "../../../input.txt";
+            var outputFilePath = "../../../output.txt";
+            var lines = File.ReadAllLines(inputFilePath);
+            var blocks = new List<Block>();
+            var currentBlock = new Block();
+            bool isInSummary = false;
+            foreach (var line in lines)
+            {
+                currentBlock.RawLines.Add(line);
+                if (line.Contains("/// <summary>"))
+                {
+                    isInSummary = true;
+                    currentBlock.IsSummary = true;
+                    currentBlock.Indent = line.Replace("/// <summary>", string.Empty);
+                }
+                else if (line.Contains("/// </summary>"))
+                {
+                    isInSummary = false;
+                    blocks.Add(currentBlock);
+                    currentBlock = new();
+                }
+                else if (isInSummary)
+                {
+                    currentBlock.SummaryBodyLines.Add(line.Replace($"{currentBlock.Indent}/// ", string.Empty));
+                }
+                else
+                {
+                    blocks.Add(currentBlock);
+                    currentBlock = new();
+                }
+            }
+
+            File.WriteAllText(outputFilePath, string.Join("\n", blocks.Select(x => x.Render())));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # CSharpSummaryShortener
 C#: 3 lines summary -> 1 line summary
+
+# Sample IO
+## Input
+```
+namespace SampleNamespace;
+public class SampleClass
+{
+    /// <summary>
+    /// SampleText A
+    /// </summary>
+    public int SampleProperty { get; set; }
+
+    /// <summary>
+    /// SampleText B
+    /// This summary is not shortened
+    /// </summary>
+    public class SampleInnerClass
+    {
+        /// <summary>
+        /// SampleText C
+        /// </summary>
+        public int SampleInnerProperty { get; set; }
+    }
+}
+```
+
+## Output
+```
+namespace SampleNamespace;
+public class SampleClass
+{
+    /// <summary> SampleText A </summary>
+    public int SampleProperty { get; set; }
+
+    /// <summary>
+    /// SampleText B
+    /// This summary is not shortened
+    /// </summary>
+    public class SampleInnerClass
+    {
+        /// <summary> SampleText C </summary>
+        public int SampleInnerProperty { get; set; }
+    }
+}
+```
